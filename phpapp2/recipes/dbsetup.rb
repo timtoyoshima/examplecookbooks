3b88b3e085e06e953ba401e58d733b6bbcdde22f
Tim Toyoshima <timtoyoshima@hotmail.com>
2012-10-26 14:46:41 -0700

    simple to work

--- phpapp2/recipes/dbsetup.rb
+++ phpapp2/recipes/dbsetup.rb
@@ -1,15 +1,5 @@
 execute "mysql-create-db" do
-  command "/usr/bin/mysql -u root CREATE DATABASE #{node['myphpapp2']['dbname']}"
+  command "/usr/bin/mysql -u root CREATE DATABASE chat"
   action :run
 end
 
-execute "mysql-create-table" do
-  command "/usr/bin/mysql -u root CREATE TABLE #{node['myphpapp2']['dbtable']} ( 
-   time int(11) NOT NULL, 
-   name varchar(30) NOT NULL, 
-   ip varchar(15) NOT NULL, 
-   message varchar(255) NOT NULL, 
-   PRIMARY KEY (time)
-  )"
-  action :run
-end
