8be138c4645ddb928708d0df971fcb782ba6673a
Tim Toyoshima <timtoyoshima@hotmail.com>
2012-10-27 23:12:38 -0700

    fix mysql command

--- phpapp2/recipes/dbsetup.rb
+++ phpapp2/recipes/dbsetup.rb
@@ -1,11 +1,11 @@
 execute "mysql-create-table" do
-  command "/usr/bin/mysql -u#{node[:deploy][:myphpapp2][:database][:username]} -p#{node[:deploy][:myphpapp2][:database][:password]} -e'CREATE TABLE chat(
+  command "/usr/bin/mysql -u#{node[:deploy][:myphpapp2][:database][:username]} -p#{node[:deploy][:myphpapp2][:database][:password]} #{node[:deploy][:myphpapp2][:database][:database]} -e'CREATE TABLE chat(
    time int(11) NOT NULL, 
    name varchar(30) NOT NULL, 
    ip varchar(15) NOT NULL, 
    message varchar(255) NOT NULL, 
    PRIMARY KEY (time)
-) #{node[:deploy][:myphpapp2][:database][:database]}'"
+)'"
   action :run
 end
 
