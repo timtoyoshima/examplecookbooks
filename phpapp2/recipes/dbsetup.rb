53dbf4936d5e44728dcdd26098e720f279376466
Tim Toyoshima <timtoyoshima@hotmail.com>
2012-10-27 19:15:56 -0700

    update for myphpapp2 and database setup

--- phpapp2/recipes/dbsetup.rb
+++ phpapp2/recipes/dbsetup.rb
@@ -1,5 +1,11 @@
-execute "mysql-create-db" do
-  command "/usr/bin/mysql -u root CREATE DATABASE chat"
+execute "mysql-create-table" do
+  command "/usr/bin/mysql -u#{node[:deploy][:myphpapp2][:database][:username]} -p#{node[:deploy][:myphpapp2][:database][:password]} -e"CREATE TABLE chat(
+   time int(11) NOT NULL, 
+   name varchar(30) NOT NULL, 
+   ip varchar(15) NOT NULL, 
+   message varchar(255) NOT NULL, 
+   PRIMARY KEY (time)
+) #{node[:deploy][:myphpapp2][:database][:database]}"
   action :run
 end
 
