533d4c13145990233de339e4d28fce11faa0e40a
timtoyoshima <timtoyoshima@hotmail.com>
2013-02-16 23:57:37 -0800

    Update phpapp/recipes/dbsetup.rb

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -1,12 +1,13 @@
-execute "mysql-create-table" do
-  command "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
-   time int(11) NOT NULL, 
-   name varchar(30) NOT NULL, 
-   ip varchar(15) NOT NULL, 
-   message varchar(255) NOT NULL, 
-   PRIMARY KEY (time)
-)'"
-  not_if "/usr/bin/mysql -u#{node[:deploy][:myphpapp][:database][:username]} -p#{node[:deploy][:myphpapp][:database][:password]} #{node[:deploy][:myphpapp][:database][:database]} -e'SHOW TABLES' | grep [:phpapp][:dbtable]"
-  action :run
+node[:deploy].each do |app_name, deploy|
+  execute "mysql-create-table" do
+    command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
+     time int(11) NOT NULL, 
+     name varchar(30) NOT NULL, 
+     ip varchar(15) NOT NULL, 
+     message varchar(255) NOT NULL, 
+     PRIMARY KEY (time)
+    )'"
+    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep [:phpapp][:dbtable]"
+    action :run
+  end
 end
-
