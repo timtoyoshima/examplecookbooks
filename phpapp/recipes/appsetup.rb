28884146d20c46e2a5a415b29db2906f2c3e7624
timtoyoshima <timtoyoshima@hotmail.com>
2013-02-16 20:18:22 -0800

    Update phpapp/recipes/appsetup.rb

--- phpapp/recipes/appsetup.rb
+++ phpapp/recipes/appsetup.rb
@@ -1,26 +1,26 @@
 log node[:phpapp][:dbtable].inspect
 
-template "#{node[:deploy][:myphpapp][:deploy_to]}/current/db-connect.php" do
-  source "db-connect.php.erb"
-  mode 0660
-  node[:deploy][:group]
+node[:deploy].each do |app_name, deploy| do
+  template "#{deploy[:deploy_to]}/current/db-connect.php" do
+    source "db-connect.php.erb"
+    mode 0660
+    node[:deploy][:group]
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
 
-if platform?("ubuntu")
-  owner "www-data"
-elsif platform?("amazon")   
-  owner "apache"
-end
+    variables(
+        :host =>     (deploy[:database][:host] rescue nil),
+        :user =>     (deploy[:database][:username] rescue nil),
+        :password => (deploy[:database][:password] rescue nil),
+        :db =>       (deploy[:database][:database] rescue nil),
+        :table =>    (node[:phpapp][:dbtable] rescue nil)
+    )
 
-
-  variables(
-      :host =>     (node[:deploy][:myphpapp][:database][:host] rescue nil),
-      :user =>     (node[:deploy][:myphpapp][:database][:username] rescue nil),
-      :password => (node[:deploy][:myphpapp][:database][:password] rescue nil),
-      :db =>       (node[:deploy][:myphpapp][:database][:database] rescue nil),
-      :table =>    (node[:phpapp][:dbtable] rescue nil)
-  )
-
- only_if do
-   File.directory?("#{node[:deploy][:myphpapp][:deploy_to]}/current")
- end
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
 end
